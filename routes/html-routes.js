ca3df930f8c7631adebcc2c66e8486c0773fd505
Jeffrey Brown <jeff@jeffbrown.us>
2017-06-05 01:01:06 -0400

    splash with login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,12 @@
 var path = require("path");
 module.exports = function(app) {
   //main page/initial load (start with login.html, change to splash as project progresses)
-  app.get("/", function(req, res) {
+  app.get("/login", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index/login.html"));
+  });
+
 };
