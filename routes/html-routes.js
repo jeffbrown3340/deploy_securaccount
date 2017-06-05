c7ec7ee2c17c77469ddcbfc806119ab89e0bc004
Jeffrey Brown <jeff@jeffbrown.us>
2017-06-05 01:03:27 -0400

    splash with login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   });
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index/login.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
 };
