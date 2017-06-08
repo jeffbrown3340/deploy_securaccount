e07995a9102662b3e8948217b9a9f25a55be3cc0
Jeffrey Brown <jeff@jeffbrown.us>
2017-06-08 02:51:38 -0400

    themed login, records path

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // static directory
 app.use(express.static("./public"));
 
+//this is a mock up of the token
+// assumes userId 2 is logged in
+app.use(function(req,res,next){
+	req.token = {userId: 3};
+	next();
+});
+
 // routes
 require("./routes/routes.js")(app);
 
